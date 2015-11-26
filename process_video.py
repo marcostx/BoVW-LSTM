7365be6b0116b9c763029af5112b0051dfb47e05
marcosvinicius <marcos.teixeira@tecsinapse.com.br>
2015-11-26 13:04:58 -0200

    ajustandooo

--- process_video.py
+++ process_video.py
@@ -16,15 +16,27 @@
 //  is trasform a video into a sequence of frames and save them in a folder appro-
 //  priate.
 
+
+// Parameters:
+// sys.args[1] => class of the video
+// sys.args[2] => name of the video
+
 """
 
-print (__doc__)
+def doc():
+ 	print (__doc__)
 
 import numpy as np
 import cv2
+import sys
 
 # Opening the video
 cap = cv2.VideoCapture('test.mp4')
+# getting the class
+clss = sys.argv[1]
+# getting the name of video
+vname = sys.argv[2]
+
 
 # Frame List
 frames_ = []
@@ -38,15 +50,14 @@ while(cap.isOpened()):
     frames_.append(frame)
 
     # Show the frame
-    # cv2.imshow('frame',frame)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+frame_count = 0
 for i in frames_:
-	cv2.imshow('frame',i)
-
-	if cv2.waitKey(20) == ord('q'):
-		exit(1)
+	cv2.imwrite('dataset/' + clss + '/' + vname + str(frame_count) + '.png',i)
+	frame_count+=1
 
 cap.release()
 cv2.destroyAllWindows()
